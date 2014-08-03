cafc419a2d9df38e8fed70122c7335876c460125
Abdelilah <Abdelilah@mail.com>
2014-08-03 15:42:15 +0000

    stable with share this

--- script/script.js
+++ script/script.js
@@ -1,32 +1,40 @@
 ﻿function getColor(d) {
-//var ar=['#FFFFB2','#FECC5C','#FD8D3C' ,'#E31A1C' ];
-var ar=['#EF0000','#A0CFEC','#FFFFFF' ,'#88419D' ];
-if (d== 'Gaza') 
-    return ar[0];
-else if (d== 'Israel') 
-    return ar[1];
-else 
-	return ar[2];
+	var ar=['#EF0000','#A0CFEC','#FFFFFF'   ];
+	if (d== 'Gaza') 
+		return ar[0];
+	else if (d== 'Israel') 
+		return ar[1];
+	else 
+		return ar[2];
 }
 
 function getalpha(d) {
-//var ar=['#FFFFB2','#FECC5C','#FD8D3C' ,'#E31A1C' ];
-var ar=[0.8,0.5,0 ,'#88419D' ];
-if (d== 'Gaza') 
-    return ar[0];
-else if (d== 'Israel') 
-    return ar[1];
-else 
-	return ar[2];
+	var ar=[0.8,0.5,0  ];
+	if (d== 'Gaza') 
+		return ar[0];
+	else if (d== 'Israel') 
+		return ar[1];
+	else 
+		return ar[2];
 }
+function get_lbl(feature){
+
+
+	if(feature.name == "Israel")
+		lbl =' <h1>' +feature.name+' </h1><br> has hosted <span class = "rfg">'+pop_label(feature.pop)+'</span> palestinian resident.' ; 
+	else if(  feature.name  =="Gaza")
+		lbl =' <h1>' +feature.name+' </h1><br> has hosted <span class = "rfg">'+pop_label(feature.pop)+'</span> palestinian resident and refugee.' ; 
+	else
+		lbl =' <h1>'+feature.name+'</h1><br> has  hosted  <span class = "rfg">'+pop_label(feature.pop)+'</span> palestinian refugee.' ; 
+	info.update(lbl) ; 
+
+}
+
 function highlightFeature(e) {
-var layer = e.target;
-var lbl ; 
-   if(layer.feature.name == "Israel" ||  layer.feature.name  =="Gaza")
-lbl =' <h1>' +layer.feature.name+' </h1><br> has hosted <span class = "rfg">'+pop_label(layer.feature.pop)+'</span> palestinian resident and refugee.' ; //+pop_label(feature.pop)
-else
-lbl =' <h1>'+layer.feature.name+'</h1><br> has  hosted  <span class = "rfg">'+pop_label(layer.feature.pop)+'</span> palestinian refugee.' ; //+pop_label(feature.pop)
-    info.update(lbl);
+	var layer = e.target;
+	
+	
+	get_lbl(layer.feature);
 }
 
 
@@ -34,18 +42,20 @@ lbl =' <h1>'+layer.feature.name+'</h1><br> has  hosted  <span class = "rfg">'+po
 function zoomToFeature(e) {
     map.fitBounds(e.target.getBounds());
 }
+
 function resetHighlight(e) {
  
     info.update('Hover over a country recieving refugees');
 }
+
 function pop_label(n){
 
-if(n < 1000)
-return Math.round(n) ; 
-else if(n < 1000000)
-return Math.round(n/1000) + "miles" ; 
-else
-return Math.round(n/1000000 * 100) / 100 + " millions" ; 
+	if(n < 1000)
+		return Math.round(n) ; 
+	else if(n < 1000000)
+		return Math.round(n/1000) + "thousands" ; 
+	else
+		return Math.round(n/1000000 * 100) / 100 + " millions" ; 
 }
 
 function style(feature) {
@@ -53,13 +63,20 @@ function style(feature) {
         fillColor:  getColor(feature.name),
         weight: 1,
         opacity: 1,
-        color: 'white',
-        //dashArray: '1',
-        fillOpacity:getalpha(feature.name)
+        color: getColor(feature.name),
+        fillOpacity: getalpha(feature.name)
     };
 }
+$(document).ready(function () {
+ $('.overlay').overlay();
+});
 
 
+
+
+
+
+$("#loading").hide();
 var initial_center = [28.536132122854514,-4.394531259];
 var initial_zoom = 2;
 var cmAttr = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade', cmUrl = 'http://{s}.tile.cloudmade.com/2796cd5ac9ce4ede8860581bcef53fba/{styleId}/256/{z}/{x}/{y}.png';
@@ -71,22 +88,23 @@ map = new L.Map('map',{
                 center: initial_center,
                 zoom: initial_zoom,
                 layers: mapLayersList     
-				//maxBounds : [[ 37.7039 , -17.1839], [ 20.7460 , -1.0658]]
+				
 });
 map.on('dblclick', reverse_geocoding) ; 
+
 //center of palestine
 var state_center_x; 
 var state_center_y; 
-        var npoints = 1000;
-        var offset = 0;
-        var coords = [];
-        var points = [];
-        var snap_tolerance = 500000;
+var country_bounds ; // = new Array() ; ;
+var labels    ; 
  
- function load_json(country)
+ function load_json(country )
  {
- //console.log(country) ; 
- 
+ if(country == "il")
+ country = "ps";
+ //console.log(country) ;
+ country_bounds = new Array()  ; 
+ labels = new Array()  ; 
  map.eachLayer(function (layer) {
   
  if(  typeof   layer.options === 'undefined'  )
@@ -98,76 +116,12 @@ var state_center_y;
 var host="" ; 
 
 if(location.host !='')
-	host  = location.host+"/"
- host="https://raw.githubusercontent.com/geogeek11/geogeek11.github.io/master/" ; 
- 
- 
- $.ajax({
-  dataType: 'jsonp',
- // data: 'id=10',
- // jsonp: 'jsonp_callback',
-  url: host+ "geojson/"+country.toUpperCase()+".json",
-  success: function(data) {
-	console.log(host+"geojson/"+country+".json") ; 
-	features=[] ; 
-	//When GeoJSON is loaded
-	var geojsonLayer = new L.GeoJSON(data , {style: style , onEachFeature: function (feature, layer) {
-         
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight ,
-		dblclick : reverse_geocoding ,
-		click : zoomToFeature
-    });
+	host  = "http://"+location.host+"/" ; 
 
+var i = 0;
 
-
-		if(typeof state_center_x === 'undefined' ||  state_center_x == null) {
-		console.log(feature.name)
-		state_center_x =  feature.x;
-		state_center_y =  feature.y;
-		//killed = feature.causalities ; 
-		setTimeout(function(){
-    odometer1.innerHTML = feature.causalities;
-							}, 0);
-		setTimeout(function(){
-    odometer2.innerHTML = feature.refugies;
-							}, 0);	
-
-	country2.innerHTML=feature.real_name ; 
-		//legend.update(feature.causalities)
-		}
-		else
-		{
-		
-		var pointA = new L.LatLng(state_center_y, state_center_x);
-var pointB = new L.LatLng(feature.y, feature.x);
-var pointList = [pointA, pointB];
-
-var firstpolyline = new L.Polyline(pointList, {
-color: 'red',
-weight: 2,
-opacity: 0.3,
-smoothFactor: 0.5 
-
-});
-firstpolyline.addTo(map);
-		
-		
-
-}
-
-var lbl ; 
-lbl =feature.name ; 
-     }
-	 });		//New GeoJSON layer
-	 geojsonLayer.addTo(map);
-	//map.addLayer(geojsonLayer);			//Add layer to map	
-state_center_x = null ; 
-}
-});
- /*
 $.getJSON( host+ "geojson/"+country.toUpperCase()+".json", function(data) {
+ //window.country_bounds
 	console.log(host+"geojson/"+country+".json") ; 
 	features=[] ; 
 	//When GeoJSON is loaded
@@ -183,10 +137,10 @@ $.getJSON( host+ "geojson/"+country.toUpperCase()+".json", function(data) {
 
 
 		if(typeof state_center_x === 'undefined' ||  state_center_x == null) {
-		console.log(feature.name)
+		//console.log(feature.name)
 		state_center_x =  feature.x;
 		state_center_y =  feature.y;
-		//killed = feature.causalities ; 
+		
 		setTimeout(function(){
     odometer1.innerHTML = feature.causalities;
 							}, 0);
@@ -200,23 +154,29 @@ $.getJSON( host+ "geojson/"+country.toUpperCase()+".json", function(data) {
 		else
 		{
 		
-		var pointA = new L.LatLng(state_center_y, state_center_x);
-var pointB = new L.LatLng(feature.y, feature.x);
-var pointList = [pointA, pointB];
+			var pointA = new L.LatLng(state_center_y, state_center_x);
+			var pointB = new L.LatLng(feature.y, feature.x);
+			var pointList = [pointA, pointB];
 
-var firstpolyline = new L.Polyline(pointList, {
-color: 'red',
-weight: 2,
-opacity: 0.3,
-smoothFactor: 0.5 
+			var firstpolyline = new L.Polyline(pointList, {
+			color: 'red',
+			weight: 2,
+			opacity: 0.3,
+			smoothFactor: 0.5 
 
-});
-firstpolyline.addTo(map);
-		
-		
+			});
+			firstpolyline.addTo(map);
 
 }
-
+ 
+if(i <3){
+country_bounds.push(layer.getBounds());
+labels.push(layer.feature);
+}
+else
+country_bounds[2].extend(layer.getBounds())
+i++;
+console.log(country_bounds.length) ; 
 var lbl ; 
 lbl =feature.name ; 
      }
@@ -224,8 +184,13 @@ lbl =feature.name ;
 	 geojsonLayer.addTo(map);
 	//map.addLayer(geojsonLayer);			//Add layer to map	
 state_center_x = null ; 
-}); */
+$("#loading").hide();
+zoom_intro() ; 
+});
+
+
 }
+
 
 var info = L.control({position: 'topright'});
  
@@ -245,6 +210,32 @@ info.addTo(map);
 
 info.update("Double click on a country to simulate");
 
+
+function zoom_intro(){
+
+
+ setTimeout(
+  function() 
+  {
+    map.fitBounds(  window.country_bounds[1] );
+	get_lbl(labels[1]);
+	 setTimeout(
+  function() 
+  {
+    map.fitBounds(  window.country_bounds[0] );
+	get_lbl(labels[0]);
+	 setTimeout(
+  function() 
+  {
+    map.fitBounds(  window.country_bounds[2] );
+	resetHighlight() ; 
+  }, 5000);
+  }, 5000);
+  }, 5000);
+
+}
+
+
 function reverse_geocoding(e){
 
 $.ajax({
@@ -252,9 +243,11 @@ $.ajax({
     url: "http://nominatim.openstreetmap.org/reverse?format=json&lat="+e.latlng.lat+"&lon="+e.latlng.lng,
     dataType: 'json',
     success: function (response) {
+	$("#loading").show();
 load_json(response.address.country_code) ; 
 
-    }
+   ;  
+ }
 });
 
 
